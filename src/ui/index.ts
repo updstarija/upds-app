d43d09b00bf91b3fd3d053a10c52dd13105d58c6
Dante Arias <danteariastarifa@gmail.com>
2023-11-10 23:59:47 -0400

    add imports

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,5 +1,6 @@
 export * from './BottomSheet';
 export { default as CustomBottomSheetModal } from './CustomBottomSheetModal';
+export { default as CustomModal } from './CustomModal';
 export { default as CustomButton } from './CustomButton';
 export * from './Modal';
 export { default as Texto } from './Texto';
