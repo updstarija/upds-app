bd05e8b9b8adacf6a4a7c990dcfbaca5c3b148c3
Dante Arias <danteariastarifa@gmail.com>
2023-11-28 00:53:09 -0400

    custom button

--- src/ui/CustomButton.tsx
+++ src/ui/CustomButton.tsx
@@ -2,9 +2,9 @@ import { useThemeColor } from '@/hooks';
 import { TouchableOpacity, TouchableOpacityProps, ActivityIndicator } from 'react-native';
 
 const variants = {
-    'primary': 'rounded-md bg-primario border-primario',
-    'primary-dark': 'rounded-md bg-secondary-dark border-primario',
-    'white': 'rounded-md bg-white border border-primario',
+    'primary': 'rounded-md bg-primario ',
+    'primary-dark': 'rounded-md bg-secondary-dark ',
+    'white': 'rounded-md bg-white  ',
     'success': 'rounded-md bg-green-500 ',
     'danger': 'rounded-md bg-red-500',
     'custom': ''
@@ -33,9 +33,11 @@ const CustomButton: React.FC<Props> = ({
     const isDark = useThemeColor() === "dark"
     return (
         <TouchableOpacity
-            className={variant === 'custom' ? className : `${variants[variant]} ${sizes[size]} w-fit`}
-            activeOpacity={isDark ? .5 : .8}
             {...props}
+
+            activeOpacity={isDark ? .5 : .8}
+            className={variant === 'custom' ? className : `${variants[variant]} ${sizes[size]} `}
+
         >
             {disabled && showLoader ?
                 <ActivityIndicator color={"red"} size={20} /> : <>{children}</>}
