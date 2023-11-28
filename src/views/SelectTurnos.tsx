394623c873b4e6f94817c95db0e4bab0115daf51
Dante Arias <danteariastarifa@gmail.com>
2023-11-28 00:53:38 -0400

    extend componente

--- src/views/SelectTurnos.tsx
+++ src/views/SelectTurnos.tsx
@@ -3,9 +3,13 @@ import clsx from 'clsx'
 import { Texto } from '@/ui'
 import { useProyeccionesContext } from '@/hooks'
 
-const SelectTurnos = () => {
-
+interface Props {
+    selectedValues?: string[]
+    handleFilterValue?: Function
+}
+const SelectTurnos: React.FC<Props> = ({ selectedValues, handleFilterValue }) => {
     const { selectedTurns, handleFilterTurn } = useProyeccionesContext()
+
     return (
         <>
             <Texto className="px-4 pb-2">Turno:</Texto>
@@ -19,14 +23,20 @@ const SelectTurnos = () => {
                     ItemSeparatorComponent={() => (
                         <View className="w-2" />
                     )}
-                    extraData={selectedTurns}
+
                     renderItem={({ item }) => (
-                        <TouchableOpacity onPress={() => handleFilterTurn(item)}>
+                        <TouchableOpacity onPress={() => {
+                            if (selectedValues && handleFilterValue) {
+                                handleFilterValue(item)
+                            } else {
+                                handleFilterTurn(item)
+                            }
+                        }}>
                             <View className={
                                 clsx([
                                     "rounded-full py-2 px-4 border-[.5px] border-primario",
                                     {
-                                        "bg-primario dark:bg-secondary-dark text-white": selectedTurns.includes(item)
+                                        "bg-primario dark:bg-secondary-dark text-white": selectedValues?.includes(item) || selectedTurns.includes(item)
                                     }
                                 ])
                             }>
@@ -34,7 +44,7 @@ const SelectTurnos = () => {
                                     clsx([
                                         "'text-black dark:text-white'",
                                         {
-                                            "text-white": selectedTurns.includes(item)
+                                            "text-white": selectedValues?.includes(item) || selectedTurns.includes(item)
                                         }
                                     ])
                                 }>{item}</Texto>
